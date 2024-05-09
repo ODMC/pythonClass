c2eb1da57a92969fb8d2b00d9144ab6bd76610f8
OLIVER <62722523+ODMC@users.noreply.github.com>
2024-05-08 23:04:54 -0500

    ejercicios propuestos resueltos

--- semana7/ejercicio2.py
+++ semana7/ejercicio2.py
@@ -6,15 +6,24 @@ funcion pasada para aplicar los descuentos o el IVA a los productos de la cesta 
 devolver el precio final de la cesta.
 '''
 # diccionario de precio con su respectivo porcentaje de descuento
-diccionario = {50:20, 30:5, 45:10}
+# diccionario = {50:20, 30:5, 45:10}
+
+def crearDiccionario():
+    diccionario={}
+    num_productos = int(input("Ingrese el número de productos: "))
+    for i in range(num_productos):
+        valor = float(input(f"Ingrese el valor del producto {i + 1}: "))
+        porcentaje = float(input(f"Ingrese el porcentaje de descuento del producto {i + 1}: "))
+        diccionario[valor] = porcentaje
+    return diccionario
+
+diccionario = crearDiccionario()
 
 def descuento(precio, porcentajeDescuento):
     return precio - precio * porcentajeDescuento / 100
 
-
 def igv(precio, porcentajeIgv):
     return precio * 18 / 100
-
 
 def funcionDiccionario(cesta, funcion):
     total = 0
@@ -22,10 +31,8 @@ def funcionDiccionario(cesta, funcion):
         total+=funcion(precio,porcentaje)
     return total
 
-
-print("El precio con descuento es " + str(funcionDiccionario(diccionario, descuento)))
 print("El igv es " + str(funcionDiccionario(diccionario, igv)))
-
+print("El precio con descuento es " + str(funcionDiccionario(diccionario, descuento)))
 
 print("El total es: " + str(funcionDiccionario(diccionario, igv)+funcionDiccionario(diccionario, descuento) ))
 
